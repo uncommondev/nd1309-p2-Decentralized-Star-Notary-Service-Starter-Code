1490a4671b4e131299dcc5232458dd6e3073935e
Uncommon Dev <uncommondev344@gmail.com>
2022-03-21 10:30:02 +0000

    Updated Truffle Config for Rinkeby
    The config has been updated but for security reasons I have removed the key path for infura and the mnemonic for my MetaMask wallet.
    
    To use this code you'll need to paste the project key from infura into the provider function in the Rinkeby section and paste your mnemonic into the variable above

--- truffle-config.js
+++ truffle-config.js
@@ -21,12 +21,8 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
-
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = "YOUR_MNEMONIC"
 
 module.exports = {
   /**
@@ -52,6 +48,14 @@ module.exports = {
       network_id: "*",       // Any network (default: none)
     },
 
+    rinkeby: {
+      provider: function() { 
+        return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/`);
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
     // Another network with more advanced options...
     // advanced: {
       // port: 8777,             // Custom port
